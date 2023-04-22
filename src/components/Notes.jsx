d42a6dd8c71f85334c477dba98b8aeb07a362fab
Gosavilatikesh <92318570+Gosavilatikesh@users.noreply.github.com>
2023-04-22 15:52:28 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div class="note">
+      <h1>Front-End Web Development Bootcamp</h1>
+      <p>
+        We learn basics of Web Development, javascript, react, reactDOM and
+        created a project at a very end of the bootcamp.
+      </p>
+    </div>
+  );
+}
+export default Notes;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading.jsx";
+import Paragraph from "./Paragraph.jsx";
+const react = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <Paragraph />
+  </div>,
+  document.getElementById("root")
+);
